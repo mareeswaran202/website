fd1d3eaa72b0e5f1d74260521f2dece4aa7ed1ea
mareeswaran202 <78137421+mareeswaran202@users.noreply.github.com>
2021-01-28 14:30:11 +0530

    nc

--- components/Contactus-ET.js
+++ components/Contactus-ET.js
@@ -7,9 +7,9 @@ function ContactET(){
          <h3>Contact Us</h3>
          <p>Excelgoodies Consulting, Inc.</p>
          <p>575 7th Avenue, 5th Floor, New York City,</p>
-         <p>NY 10018​​​​​​​</p>
+         <p>NY 10018​​</p>
          <h6>Mr.Paul Melton</h6>
-         <p><img src="/images/Phone.png" className={styles.top_logo}></img><span>Tel: (347) 790-1311</span></p>
+         <p><img src="/images/phone.png" className={styles.top_logo}></img><span>Tel: (347) 790-1311</span></p>
          <p className={"mt-3"}> <img src="/images/texas_top_mail.png" className={styles.top_logo}></img><span>inquiry_cambridge@excelgoodies.com</span></p>
      </div>
     );
